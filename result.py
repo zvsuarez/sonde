824f0a65c056fe8477daf2adf6e6c33b1542cf76
Zyrolie Suárez <zyrvsuarez07@gmail.com>
2021-12-01 23:13:56 +0800

    added flexible dimensions of qtableview and qdialog itself

--- result.py
+++ result.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/decode_window.ui'
+# Form implementation generated from reading ui file 'ui/result.ui'
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from openpyxl import Workbook
-import os
 
 class TableView(QtWidgets.QTableWidget):
-    def __init__(self, data, *args):
-        QtWidgets.QTableWidget.__init__(self, *args)
-        self.setGeometry(QtCore.QRect(0, 0, 1060, 760))
-        self.data = data
-        self.setData()
-        self.resizeColumnsToContents()
-        self.resizeRowsToContents()
+
     def setData(self):
         headers = []
         for n, key in enumerate(self.data.keys()):
@@ -24,11 +17,47 @@ class TableView(QtWidgets.QTableWidget):
                 self.setItem(m, n, newItem)
         self.setHorizontalHeaderLabels(headers)
 
+    def setDimensions_col(self):
+        total_col = 0
+        for column in range(QtWidgets.QTableWidget.columnCount(self)):
+            total_col += QtWidgets.QTableView.columnWidth(self,column)
+
+        if total_col > 1280:
+            return 1280
+        if total_col % 2 != 0:
+            return (total_col - 1) + 40
+        else:
+            return total_col + 40
+
+    def setDimensions_row(self):
+        total_row = 0
+        for row in range(QtWidgets.QTableWidget.rowCount(self)):
+            total_row += QtWidgets.QTableView.rowHeight(self,row)
+
+        if total_row > 720:
+            return 720
+        if total_row % 2 != 0:
+            return (total_row - 1) + 25
+        else:
+            return total_row + 25
+        
+    def __init__(self, data, *args):
+        QtWidgets.QTableWidget.__init__(self, *args)
+        self.data = data
+        self.setData()
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
+        col = self.setDimensions_col()
+        row = self.setDimensions_row()
+        self.setGeometry(QtCore.QRect(0, 0, col, row))
+        #print(col, row)
+
+
 class Ui_Dialog(object):
 
     def save_result(self):
         dialog = QtWidgets.QFileDialog()
-        path = dialog.getSaveFileName(QtWidgets.QMainWindow(), 'Save File...', '', filter='Excel Files (*xlsx *xls)')
+        path = dialog.getSaveFileName(QtWidgets.QMainWindow(), 'Save File...', '', filter='Excel File (*.xlsx *.xls)')
         wb = Workbook()
         ws = wb.active
 
@@ -48,29 +77,30 @@ class Ui_Dialog(object):
 
         except FileNotFoundError:
             pass
-        
+
     def setupUi(self, Dialog, data, *args):
+        self.tableWidget = TableView(data, *args, Dialog)
+        self.tableWidget.setObjectName("tableWidget")
+        #self.tableWidget = QtWidgets.QTableWidget(Dialog)
+        #self.tableWidget.setGeometry(QtCore.QRect(0, 0, 1280, 660))
+        #self.tableWidget.setColumnCount(0)
+        #self.tableWidget.setRowCount(0)
         Dialog.setObjectName("Dialog")
-        Dialog.setFixedSize(1060, 820)
+        Dialog.setFixedSize(self.tableWidget.setDimensions_col(), self.tableWidget.setDimensions_row()+80)
         Dialog.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/icons/32x32.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/icons/ui/icons/32x32.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/icons/ui/icons/32x32.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
         Dialog.setWindowIcon(icon)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(440, 770, 200, 41))
+        self.buttonBox.setGeometry(QtCore.QRect((self.tableWidget.setDimensions_col() // 2)-100, self.tableWidget.setDimensions_row() + ((self.tableWidget.setDimensions_row()+60 - self.tableWidget.setDimensions_row()) / 2) - 10, 200, 40))
         font = QtGui.QFont()
         font.setFamily("MS Shell Dlg 2")
         self.buttonBox.setFont(font)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Save)
         self.buttonBox.setObjectName("buttonBox")
-        self.tableWidget = TableView(data, *args, Dialog)
-        #self.tableWidget = QtWidgets.QTableWidget(Dialog)
-        #self.tableWidget.setGeometry(QtCore.QRect(0, 0, 1060, 760))
-        self.tableWidget.setObjectName("tableWidget")
-        #self.tableWidget.setColumnCount(0)
-        #self.tableWidget.setRowCount(0)
-
+        
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(self.save_result)
         self.buttonBox.rejected.connect(Dialog.reject)
